af78b1d8bc74211706db05a24779ac2f23e52ba8
Muhammad Asif <snew3110@gmail.com>
2020-10-02 15:19:55 +0500

    production compiled code

---
+++ build/precache-manifest.b51e73c4649896cd536a28aeacd9f92a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cd36f0efd83c9f518b768c69b44864a5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e1c7794c57c5fbc5747e",
+    "url": "/static/css/main.6d26ece4.chunk.css"
+  },
+  {
+    "revision": "b569768731f2bd3b7fe7",
+    "url": "/static/js/2.a47d58ce.chunk.js"
+  },
+  {
+    "revision": "4db0b4b7fd2c7a0aaf4845dcd6ed7f11",
+    "url": "/static/js/2.a47d58ce.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e1c7794c57c5fbc5747e",
+    "url": "/static/js/main.76ec5ff7.chunk.js"
+  },
+  {
+    "revision": "4040b55ff6b868e876aa",
+    "url": "/static/js/runtime-main.2c219ed3.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
